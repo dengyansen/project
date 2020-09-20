48b22c13b43383bd5e393b50966e2bb99ed66b1c
dengyansen <739049105@qq.com>
2020-09-20 22:47:42 +0800

    init

---
+++ src/test/java/com/yansen/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yansen.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
